0922c56393c7d32b3036204902c1a84ec624b329
Wasiur Rahman <wrahman0@gmail.com>
2014-09-15 16:04:21 -0400

    Pre

--- gen/com/hack/clipper/R.java
+++ gen/com/hack/clipper/R.java
@@ -11,7 +11,13 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int ic_action_search=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+    }
+    public static final class id {
+        public static final int LinearLayout1=0x7f060000;
+        public static final int editText1=0x7f060002;
+        public static final int searchBar=0x7f060001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -19,6 +25,7 @@ public final class R {
     public static final class string {
         public static final int app_name=0x7f040000;
         public static final int hello_world=0x7f040001;
+        public static final int searchHint=0x7f040002;
     }
     public static final class style {
         /** 
