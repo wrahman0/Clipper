b7b4a6264ea5eb1203e4371471ed9709735e0ed3
Ajanthan Hariharan <ajanthanh@gmail.com>
2014-09-15 18:21:26 -0400

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	bin/classes/com/hack/clipper/R$id.class
    	bin/classes/com/hack/clipper/R$layout.class
    	bin/classes/com/hack/clipper/R$string.class
    	gen/com/hack/clipper/R.java

--- src/com/hack/api/Tinycc.java
+++ src/com/hack/api/Tinycc.java
@@ -1,7 +1,49 @@
 package com.hack.api;
 
-public class Tinycc {
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+import android.util.Log;
+
+import com.hack.clipper.MainActivity;
+import com.hack.network.OnPostParseInterface;
+import com.hack.network.RequestClippingService;
+
+
+public class Tinycc extends URLShortener{
 	
+	private static final String API_KEY = "a4df9f72-eb34-4b35-b2e8-a2d0eea6b461";
+	private static final String URL_FIRST = "http://tiny.cc/?c=rest_api&m=shorten&version=2.0.3&format=json&longUrl=";
+	private static final String URL_SECOND = "&login=wrahman&apiKey=";
 	
+	private OnPostParseInterface listener;
+	
+	public Tinycc (OnPostParseInterface listener){
+		this.listener = listener;
+	}
+	
+	@Override
+	public String clipURL(String url) {
+		String encoded_url;
+		try {
+			url = validateUrl(url);
+			encoded_url = URLEncoder.encode(url, "utf-8");
+			Log.e(MainActivity.TAG, "SHORTENING: " + encoded_url);
+		} catch (UnsupportedEncodingException e) {
+			Log.e(MainActivity.TAG,"Unable to encode URL");
+			e.printStackTrace();
+			return null;
+		}
+		RequestClippingService requestClippingService = new RequestClippingService(listener);
+		requestClippingService.execute(URL_FIRST + encoded_url + URL_SECOND + API_KEY);
+		return null;
+	}
+	
+	private String validateUrl(String url){
+		if (url.indexOf("http") != 0){
+			url = "http://" + url;
+		}
+		return url;
+	}
 
 }
--- src/com/hack/clipper/MainActivity.java
+++ src/com/hack/clipper/MainActivity.java
@@ -1,18 +1,120 @@
 package com.hack.clipper;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+import com.hack.api.Tinycc;
+import com.hack.network.OnPostParseInterface;
 
+public class MainActivity extends Activity implements OnPostParseInterface{
+	
+	public static final String TAG = "Clipper";
+	
+	private String[] service = {"tinycc", "bitly", "googly"};
+	private String currentService;
+	
+	private TextView tinycc;
+	private TextView bitly;
+	private TextView googly;
+	private ImageButton mSearch;
+	private EditText mSearchEditText;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tinycc = (TextView) findViewById(R.id.tinyccView);
+        bitly = (TextView) findViewById(R.id.bitlyView);
+        googly = (TextView) findViewById(R.id.googlyView);
+        mSearch = (ImageButton) findViewById(R.id.search);
+        mSearchEditText = (EditText) findViewById(R.id.searchEditText);
         
+        tinycc.setOnClickListener(new ClippingServiceClickListener());
+        bitly.setOnClickListener(new ClippingServiceClickListener());
+        googly.setOnClickListener(new ClippingServiceClickListener());
         
+        //Default Service is TinyCC
+        setService(service[0]);
         
+        mSearch.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				String searchQuery = getSearchContent();
+				if (currentService == service[0] && searchQuery != null){
+					Tinycc tiny = new Tinycc(MainActivity.this);
+					tiny.clipURL(searchQuery);
+				}else if (currentService == service[1] && searchQuery != null){
+					
+				}else if (currentService == service[2] && searchQuery != null){
+					
+				}
+			}
+		});
     }
+    
+    private String getSearchContent(){
+    	return mSearchEditText.getText().toString().isEmpty() ? "":mSearchEditText.getText().toString();
+    }
+    
+    private void setService (String service){
+    	tinycc.setBackgroundColor(Color.TRANSPARENT);
+    	bitly.setBackgroundColor(Color.TRANSPARENT);
+    	googly.setBackgroundColor(Color.TRANSPARENT);
+    	
+    	if(service==this.service[0]){
+    		tinycc.setBackgroundColor(Color.GRAY);
+    		currentService = this.service[0];
+    	}else if(service==this.service[1]){
+    		bitly.setBackgroundColor(Color.GRAY);
+    		currentService = this.service[1];
+    	}else if (service==this.service[2]){
+    		googly.setBackgroundColor(Color.GRAY);
+    		currentService = this.service[2];
+    	}
+    	
+    }
+    
+    private class ClippingServiceClickListener implements OnClickListener {
+        @Override
+        public void onClick(View v) {
+        	switch(v.getId()){
+        	case R.id.tinyccView:
+        		setService(service[0]);
+        		break;
+        	case R.id.bitlyView:
+        		setService(service[1]);
+        		break;
+        	case R.id.googlyView:
+        		setService(service[2]);
+        		break;
+        	}
+        }
+    }
+    
+
+	@Override
+	public void onPostExecute(String result) {
+		try {
+			JSONObject json = new JSONObject(result);
+			String clipped_url=json.getJSONObject("results").getString("short_url");
+			Toast.makeText(getBaseContext(), clipped_url, Toast.LENGTH_LONG).show();
+		} catch (JSONException e) {
+			Log.e(MainActivity.TAG, "Unable to construct a JSON object");
+			e.printStackTrace();
+			return;
+		}
+	}
 }
